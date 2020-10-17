171362bc0a03b72132c6403d02b4b15390f0b371
Idriss Dimson <idrissdimson@macbook-pro.myfiosgateway.com>
2020-10-17 16:38:47 -0400

    start of new web identity

--- js/index.js
+++
@@ -1,6 +0,0 @@
-let navBar = document.getElementById("navFun");
-let navBarToggle = document.getElementById("button");
-
-navBarToggle.addEventListener("click", function() {
- navBar.classList.toggle("active");
-});
